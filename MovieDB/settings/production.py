beee951ddfa8980dc517ce8b32390650e8fc765e
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-12-01 11:21:13 +0700

    ubah production allowed host 57

--- MovieDB/settings/production.py
+++ MovieDB/settings/production.py
@@ -15,5 +15,5 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = "/home/wwwachm1/HORROR/staticfiles"
-MEDIA_ROOT = '/home/wwwachm1/HORROR/mediafiles'
+STATIC_ROOT = "/home/wwwachm1/public_horror-db/static"
+MEDIA_ROOT = /home/wwwachm1/public_horror-db/media"
